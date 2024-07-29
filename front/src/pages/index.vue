5992aa494d52bd285d99caf41b74cf1977f553a1
root <root@LAPTOP-8HJ036PG>
2024-07-29 12:30:57 +0300

    Tables are styled

--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="align-center hero-unit">
     <div>
+  <v-table class="d-table">
+    <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Calories
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in desserts"
+        :key="item.name"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+      </tr>
+    </tbody>
+  </v-table>
     </div>
   </div>
 </template>
@@ -10,15 +31,51 @@
 
 <script>
 export default {
-  data () {
-    return {
-      check: true,
-      textField: undefined,
-      select: undefined,
-      selects: ['One', 'Two', 'Three'],
-      date: new Date(), 
-      tab: undefined
-    }
-  }
+ data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+      }
+    },
 }
 </script>
