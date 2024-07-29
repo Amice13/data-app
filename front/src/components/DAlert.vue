9c97d5a75e0e904af7ccf45065301568e533ffcd
root <root@LAPTOP-8HJ036PG>
2024-07-29 10:00:17 +0300

    Alert component is added

---
+++ front/src/components/DAlert.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="diia-alert">
+    <div v-show="showIcon" class="flex-shrink-0 flex-grow-0 mr-4">
+      <img :src="`${assets}/emoji/${icon}`" />
+    </div>
+    <div class="main-text flex-grow-1">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+
+.diia-alert {
+  background: #fff7e3;
+  display: flex;
+  padding: 16px 24px;
+  color: #000;
+  border-image-slice: 1;
+  border-style: solid;
+  border-width: 2px;
+  border-image-source: linear-gradient(0deg, #2833d0 .01%, #7179ef 17.19%, #bf98c6 48.44%, #f1acac);
+}
+
+</style>
+
+<script>
+export default {
+  props: {
+    showIcon: {
+      type: Boolean
+    },
+    icon: {
+      type: String,
+      default: 'finger.png'
+    }
+  },
+  computed: {
+    assets () {
+      return process.env.ROOT_URL || ''
+    }
+  }
+}
+</script>
--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,22 +1,22 @@
 <template>
   <div style="height: vh100;" class="d-flex align-center hero-unit">
-    <div class="diia-alert">Text</div>
+    <div>
+      <div class="tabs d-flex">
+        <div class="tab">
+          Послуги
+        </div>
+        <div class="tab">
+          Відомості про мене
+        </div>
+      </div>
+      <div>Hello</div>
+    </div>
+    <d-alert>Hello</d-alert>
     <d-btn animated>Увійти</d-btn>
   </div>
 </template>
 
 <style>
-
-.diia-alert {
-  background: #fff7e3;
-  display: flex;
-  padding: 24px 32px;
-  color: #000;
-  border-image-slice: 1;
-  border-style: solid;
-  border-width: 2px;
-  border-image-source: linear-gradient(0deg, #2833d0 .01%, #7179ef 17.19%, #bf98c6 48.44%, #f1acac);
-}
 
 .border-color {
   display: flex;
@@ -34,6 +34,7 @@
   border-image: linear-gradient(90deg,#c3aab2 0,#9ec 57.66%,#80c0c8 76.7%,#4b8bfa 100%);
   border-image-slice: 1
 }
+
 .hero-unit {
   display: flex;
   justify-content: center;
